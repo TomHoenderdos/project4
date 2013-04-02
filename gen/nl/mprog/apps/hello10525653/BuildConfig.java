0957eb616a4b58f598b458b7f85763212105f438
Tom <tom@wcw-stud-249-232.wireless.uva.nl>
2013-04-02 16:36:31 +0200

    Initial commit with working files

---
+++ gen/nl/mprog/apps/hello10525653/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package nl.mprog.apps.hello10525653;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/nl/mprog/apps/hello10525653/MainActivity.java
@@ -0,0 +1,27 @@
+package nl.mprog.apps.hello10525653;
+
+// Name: Tom Hoenderdos
+// Email: info@tompc.nl
+// ID: 10525653
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+    
+}
